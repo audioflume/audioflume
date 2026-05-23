e1327a01fc4a03fc476d34260eb702567c7d731c
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-23 04:55:12 -0700

    Reduce modal close icon size

--- components/ModalShell.tsx
+++ components/ModalShell.tsx
@@ -89,8 +89,8 @@ export default function ModalShell({
           aria-label={closeLabel}
         >
           <svg
-            width="22"
-            height="22"
+            width="18"
+            height="18"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
