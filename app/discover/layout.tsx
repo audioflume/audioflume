9a17323ba9a045919e19a10f315c6321b4c90528
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-24 03:37:18 -0700

    Delete excessive discover layout file

--- app/discover/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import "./ready-track-corners.css";
-
-export default function DiscoverLayout({ children }: { children: ReactNode }) {
-  return children;
-}
