1de5764d2b8b8381021b1fe25323699d869ab0d9
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-25 13:33:52 -0700

    Remove music banner cover helper

--- app/music/layout.tsx
+++ app/music/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import MusicBannerCoverFix from "@/components/MusicBannerCoverFix";
-
 export default function MusicLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <MusicBannerCoverFix />
-    </>
-  );
+  return children;
 }
