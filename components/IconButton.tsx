eecec81a3e918bf26c9842d2c6e8255ff6367947
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-26 15:34:11 -0700

    Revert active icon color variable change

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -24,8 +24,8 @@ export default function IconButton({
   const style =
     active && usesPlainActiveIcon
       ? ({
-          "--edit-points-icon-color": "var(--text-primary)",
-          "--favorite-icon-color": "var(--text-primary)",
+          "--edit-points-icon-color": "#000000",
+          "--favorite-icon-color": "#000000",
         } as CSSProperties)
       : undefined;
 
