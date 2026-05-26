0c31e59bbaa75266bf39d02a3fe6655fb5c8ca9e
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-26 07:07:45 -0700

    Initialize Tauri deep link plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -40,6 +40,7 @@ fn open_path(path: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
