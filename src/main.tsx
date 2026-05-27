dc6a73f66b2dc1a2fb2a1a25853f90ea0daf7ede
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-27 14:42:43 -0700

    Stop remounting app after realtime sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { startRealtimeSyncController } from "./lib/realtimeSyncController";
 
-const REALTIME_UPDATED_EVENT = "filmwave:realtime-projects-updated";
-
 startRealtimeSyncController();
-
-function Root() {
-  const [appKey, setAppKey] = useState(0);
-
-  useEffect(() => {
-    function handleRealtimeProjectsUpdated() {
-      setAppKey((current) => current + 1);
-    }
-
-    window.addEventListener(REALTIME_UPDATED_EVENT, handleRealtimeProjectsUpdated);
-
-    return () => {
-      window.removeEventListener(REALTIME_UPDATED_EVENT, handleRealtimeProjectsUpdated);
-    };
-  }, []);
-
-  return <App key={appKey} />;
-}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
 );
