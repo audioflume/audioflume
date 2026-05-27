7a30aeca06f5e5bdc25c82317c13cfe121480744
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-27 14:35:50 -0700

    Refresh desktop UI after realtime sync updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { startRealtimeSyncController } from "./lib/realtimeSyncController";
 
+const REALTIME_UPDATED_EVENT = "filmwave:realtime-projects-updated";
+
 startRealtimeSyncController();
+
+function Root() {
+  const [appKey, setAppKey] = useState(0);
+
+  useEffect(() => {
+    function handleRealtimeProjectsUpdated() {
+      setAppKey((current) => current + 1);
+    }
+
+    window.addEventListener(REALTIME_UPDATED_EVENT, handleRealtimeProjectsUpdated);
+
+    return () => {
+      window.removeEventListener(REALTIME_UPDATED_EVENT, handleRealtimeProjectsUpdated);
+    };
+  }, []);
+
+  return <App key={appKey} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
