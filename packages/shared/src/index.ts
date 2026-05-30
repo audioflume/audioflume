eeb47aa2aea1a7aa2c37c25061ba65f10b47d511
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-29 22:02:18 -0700

    feat: export shared waveform canvas

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,5 @@ export * from "./types";
 export * from "./waveform";
 export { default as MusicPlayerShell } from "./MusicPlayerShell";
 export type { MusicPlayerShellLayout } from "./MusicPlayerShell";
+export { default as SharedWaveformCanvas } from "./SharedWaveformCanvas";
+export type { SharedWaveformCanvasProps } from "./SharedWaveformCanvas";
