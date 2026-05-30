e1f6c39a4797157d402b36b340e6ab130bc14505
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-29 18:36:50 -0700

    feat: export shared music player

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./types";
 export * from "./waveform";
+export { default as SharedMusicPlayer } from "./SharedMusicPlayer";
+export type { SharedMusicPlayerLayout } from "./SharedMusicPlayer";
