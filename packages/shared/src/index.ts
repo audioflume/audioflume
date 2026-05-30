a03d8243336b7e1f0b83dd5a651e8f8a54dc9e88
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-30 01:01:49 -0700

    feat: export shared sidebar primitives

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,4 @@ export { default as MusicPlayerShell } from "./MusicPlayerShell";
 export type { MusicPlayerShellLayout } from "./MusicPlayerShell";
 export { default as SharedWaveformCanvas } from "./SharedWaveformCanvas";
 export type { SharedWaveformCanvasProps } from "./SharedWaveformCanvas";
+export * from "./SidebarShell";
