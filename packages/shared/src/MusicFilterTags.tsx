17e124f2acf6c8d508d45e0f4540ece6fe3786a9
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-31 16:51:01 -0700

    Add shared music filter tags presentation

---
+++ packages/shared/src/MusicFilterTags.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { SearchFilterTag, SearchFilterTagList } from "./SearchFilterChrome";
+
+export type MusicFilterTagItem = {
+  id: string;
+  label: string;
+  icon?: ReactNode;
+  onRemove: () => void;
+};
+
+export function MusicFilterTags({ tags }: { tags: MusicFilterTagItem[] }) {
+  if (tags.length === 0) return null;
+
+  return (
+    <SearchFilterTagList>
+      {tags.map((tag) => (
+        <SearchFilterTag key={tag.id} icon={tag.icon} onRemove={tag.onRemove}>
+          {tag.label}
+        </SearchFilterTag>
+      ))}
+    </SearchFilterTagList>
+  );
+}
