4e65cd1824cbad086d1b8093889a0788dc27215b
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-05-31 23:44:20 -0700

    Export shared song stem helpers

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,6 +4,7 @@ export * from "./playbackControls";
 export * from "./musicFilters";
 export * from "./musicLibraryFiltering";
 export * from "./editPointUtils";
+export * from "./songStems";
 export { default as MusicPlayerShell } from "./MusicPlayerShell";
 export type { MusicPlayerShellLayout } from "./MusicPlayerShell";
 export { default as SharedWaveformCanvas } from "./SharedWaveformCanvas";
