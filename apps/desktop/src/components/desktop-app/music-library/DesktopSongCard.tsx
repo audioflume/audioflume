34fa4f56a06045d90861079ba452c5b00841750d
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-01 02:58:22 -0700

    Increase desktop synced song icon size

--- apps/desktop/src/components/desktop-app/music-library/DesktopSongCard.tsx
+++ apps/desktop/src/components/desktop-app/music-library/DesktopSongCard.tsx
@@ -280,7 +280,7 @@ function PauseIcon() {
 
 function SyncedFileIcon() {
   return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
       <rect
         x="3.75"
         y="4.25"
