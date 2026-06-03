2b6c3d75143cb406ba2554f4186ee98591389160
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-02 18:15:26 -0700

    Export shared dropdown shell

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -9,6 +9,7 @@ export * from "./musicPlaylistIdentity";
 export * from "./MusicIcons";
 export { default as MusicPlayerShell } from "./MusicPlayerShell";
 export type { MusicPlayerShellLayout } from "./MusicPlayerShell";
+export { default as DropdownShell } from "./DropdownShell";
 export { default as SharedWaveformCanvas } from "./SharedWaveformCanvas";
 export type { SharedWaveformCanvasHandle, SharedWaveformCanvasProps } from "./SharedWaveformCanvas";
 export * from "./SidebarShell";
