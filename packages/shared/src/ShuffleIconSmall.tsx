7974ae9dd73d626350a57d5b7dc8af6f7eb46105
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-04 03:23:50 -0700

    Increase shared shuffle icon stroke weight

--- packages/shared/src/ShuffleIconSmall.tsx
+++ packages/shared/src/ShuffleIconSmall.tsx
@@ -14,26 +14,26 @@ export function ShuffleIconSmall({ size = 15 }: ShuffleIconSmallProps) {
       <path
         d="M0,316.54h74.19c45.57,0,87.69-24.27,110.54-63.7l60.01-103.56c22.85-39.43,64.97-63.7,110.54-63.7h94.1"
         stroke="currentColor"
-        strokeWidth="32"
+        strokeWidth="38"
         strokeMiterlimit="10"
       />
       <polyline
         points="375.1 7.78 452.6 85.28 375.1 162.78"
         stroke="currentColor"
-        strokeWidth="32"
+        strokeWidth="38"
         strokeMiterlimit="10"
         fill="none"
       />
       <path
         d="M0,85.57h74.19c45.57,0,87.69,24.27,110.54,63.7l60.01,103.56c22.85,39.43,64.97,63.7,110.54,63.7h94.1"
         stroke="currentColor"
-        strokeWidth="32"
+        strokeWidth="38"
         strokeMiterlimit="10"
       />
       <polyline
         points="375.1 394.33 452.6 316.83 375.1 239.33"
         stroke="currentColor"
-        strokeWidth="32"
+        strokeWidth="38"
         strokeMiterlimit="10"
         fill="none"
       />
