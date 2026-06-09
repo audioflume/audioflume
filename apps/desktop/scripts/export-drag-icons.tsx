f447a81f2e65fd4614279d063f13e72d3428a027
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-09 15:54:22 -0700

    Repair desktop drag icon export script

--- apps/desktop/scripts/export-drag-icons.tsx
+++ apps/desktop/scripts/export-drag-icons.tsx
@@ -1,15 +1,9 @@
-// @ts-nocheck
-
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
 import sharp from "sharp";
 import { mkdir } from "node:fs/promises";
 import path from "node:path";
 
-import FolderIcon from "../src/components/icons/FolderIcon";
-import MusicIcon from "../src/components/icons/MusicIcon";
-
 const outputDir = path.resolve(process.cwd(), "src-tauri/icons");
 
-function withSize(svg: string) {
-  const close
+const folderSvg = `
+<svg width="96" height="96" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M4 7.5C4 6.67157 4.67157 6 5.5 6H9.4L11.1 8H18.5C19.3284 8 20 8.67157 20 9.5V17.5C20
