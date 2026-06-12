c625e1615b2815a5c9347b722d90e80e02accdb2
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-12 14:11:28 -0700

    Enlarge desktop edit point flag icon

--- apps/desktop/src/components/icons/EditPointsIcon.tsx
+++ apps/desktop/src/components/icons/EditPointsIcon.tsx
@@ -18,7 +18,7 @@ export default function EditPointsIcon({ size = 14 }: EditPointsIconProps) {
         strokeLinecap="round"
       />
       <path
-        d="M7 5.25L17 8L7 10.75V5.25Z"
+        d="M7 4.25L18.5 8L7 11.75V4.25Z"
         stroke="var(--edit-points-icon-color, currentColor)"
         strokeWidth="1.9"
         strokeLinejoin="round"
