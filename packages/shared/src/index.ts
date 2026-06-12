7531f7aa36baff76502c91a98fa3ee5c20593fa7
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-12 12:27:54 -0700

    Fix: remove duplicate MusicFilterPanel/MusicLibraryToolbar exports that broke the shared module

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -37,7 +37,3 @@ export * from "./SettingsSideNav";
 export * from "./CollapsibleSearchPill";
 export * from "./ProjectBrowserGlyphs";
 export * from "./MusicLibraryRedesign";
-export {
-  MusicFilterPanel,
-  MusicLibraryToolbar,
-} from "./MusicLibraryFilterDropdownTags";
