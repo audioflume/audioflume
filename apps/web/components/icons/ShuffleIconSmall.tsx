c7d92b7790b87a3db64f52743e84a53cb3f12fd8
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-12 14:12:16 -0700

    Redraw web shuffle icon

--- apps/web/components/icons/ShuffleIconSmall.tsx
+++ apps/web/components/icons/ShuffleIconSmall.tsx
@@ -2,40 +2,42 @@ type ShuffleIconSmallProps = {
   size?: number;
 };
 
-export default function ShuffleIconSmall({ size = 15 }: ShuffleIconSmallProps) {
+export default function ShuffleIconSmall({ size = 14 }: ShuffleIconSmallProps) {
   return (
     <svg
       width={size}
       height={size}
-      viewBox="0 0 468.15 402.11"
+      viewBox="0 0 24 24"
       fill="none"
       aria-hidden="true"
     >
       <path
-        d="M0,316.54h74.19c45.57,0,87.69-24.27,110.54-63.7l60.01-103.56c22.85-39.43,64.97-63.7,110.54-63.7h94.1"
+        d="M4.25 7.25H6.35C7.7 7.25 8.88 7.95 9.6 9.1L14.4 16.9C15.12 18.05 16.3 18.75 17.65 18.75H19.75"
         stroke="currentColor"
-        strokeWidth="26"
-        strokeMiterlimit="10"
-      />
-      <polyline
-        points="375.1 7.78 452.6 85.28 375.1 162.78"
-        stroke="currentColor"
-        strokeWidth="26"
-        strokeMiterlimit="10"
-        fill="none"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
-        d="M0,85.57h74.19c45.57,0,87.69,24.27,110.54,63.7l60.01,103.56c22.85,39.43,64.97,63.7,110.54,63.7h94.1"
+        d="M4.25 16.75H6.35C7.7 16.75 8.88 16.05 9.6 14.9L14.4 7.1C15.12 5.95 16.3 5.25 17.65 5.25H19.75"
         stroke="currentColor"
-        strokeWidth="26"
-        strokeMiterlimit="10"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
-      <polyline
-        points="375.1 394.33 452.6 316.83 375.1 239.33"
+      <path
+        d="M17.25 2.9L19.75 5.25L17.25 7.6"
         stroke="currentColor"
-        strokeWidth="26"
-        strokeMiterlimit="10"
-        fill="none"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.25 16.4L19.75 18.75L17.25 21.1"
+        stroke="currentColor"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
