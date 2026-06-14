7bd06fdc3b4f9ffe1ced4652555f4a0a94c243b0
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-13 17:22:21 -0700

    Mount region field on new song admin page

--- apps/web/app/admin/songs/new/page.tsx
+++ apps/web/app/admin/songs/new/page.tsx
@@ -1,10 +1,12 @@
 import AdminSongForm from '@/components/admin/AdminSongForm'
 import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
+import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
 
 export default function NewSongPage() {
   return (
     <>
       <AdminSongForm mode="create" />
+      <AdminSongRegionFieldInjector />
       <AdminSongFormEditPointsLinkInjector />
     </>
   )
