e0082951042c5cd2863c22020d542300bb69beb4
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-06-14 21:56:17 -0700

    Export shared side filter behavior

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -37,5 +37,6 @@ export * from "./SettingsSideNav";
 export * from "./CollapsibleSearchPill";
 export * from "./ProjectBrowserGlyphs";
 export * from "./MusicLibraryRedesign";
+export { default as SideFilterPanelBehavior } from "./SideFilterPanelBehavior";
 export { MusicLibraryToolbar } from "./MusicLibraryToolbar";
 export type { MusicLibraryToolbarProps } from "./MusicLibraryToolbar";
