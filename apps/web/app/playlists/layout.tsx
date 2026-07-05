bf7bb6f9685a77fdba1613874c37b3d24015c51e
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-05 14:47:51 -0700

    Remove playlists secondary tabs from layout

--- apps/web/app/playlists/layout.tsx
+++ apps/web/app/playlists/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
-import PlaylistTabsRail from "./PlaylistTabsRail";
-import "./playlists-tabs-rail.css";
 
 export default function PlaylistsLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <PlaylistTabsRail />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
