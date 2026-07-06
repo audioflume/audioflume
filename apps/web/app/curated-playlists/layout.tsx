5a74025a6d8010c0016f487e56240d70dffc2b02
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-05 17:19:41 -0700

    Load curated playlists route styles

---
+++ apps/web/app/curated-playlists/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./curated-playlists.css";
+
+export default function CuratedPlaylistsLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
