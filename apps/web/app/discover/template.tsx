233e99073dc8341e2e12b66e84d4bf0af4836c36
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-10 03:49:31 -0700

    Unmount discover search hero block

--- apps/web/app/discover/template.tsx
+++ apps/web/app/discover/template.tsx
@@ -1,16 +1,9 @@
 import type { ReactNode } from "react";
-import DiscoverSearchHeroPortal from "./DiscoverSearchHeroPortal";
 import SuggestedForYouPortal from "./SuggestedForYouPortal";
 
 export default function DiscoverTemplate({ children }: { children: ReactNode }) {
   return (
     <>
-      <style>{`
-        .discover-header-search-row {
-          display: none !important;
-        }
-      `}</style>
-      <DiscoverSearchHeroPortal />
       <SuggestedForYouPortal />
       {children}
     </>
