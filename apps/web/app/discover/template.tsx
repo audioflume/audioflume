7238c7e54139d7e1c2d4dfc3634ed1f0291d3f09
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-10 03:34:10 -0700

    Mount discover search hero portal

--- apps/web/app/discover/template.tsx
+++ apps/web/app/discover/template.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from "react";
+import DiscoverSearchHeroPortal from "./DiscoverSearchHeroPortal";
 import SuggestedForYouPortal from "./SuggestedForYouPortal";
 
 export default function DiscoverTemplate({ children }: { children: ReactNode }) {
   return (
     <>
+      <DiscoverSearchHeroPortal />
       <SuggestedForYouPortal />
       {children}
     </>
