d77d14e448f45f2da26baabdf1cc212ce579e1da
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-13 16:56:45 -0700

    Remove legacy curated hero top offset

--- apps/web/app/curated-playlists/CuratedHeaderScrollState.tsx
+++ apps/web/app/curated-playlists/CuratedHeaderScrollState.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 
 const CURATED_SCROLLED_CLASS = "filmwave-curated-scrolled";
 const CURATED_SCROLL_THRESHOLD = 18;
 const HEADER_MENU_OPEN_SELECTOR = ".filmwave-header-nav-item-playlists.is-open";
 
 export default function CuratedHeaderScrollState() {
-  useEffect(() => {
+  useLayoutEffect(() => {
     let frame = 0;
+    const pageLayer = document.querySelector<HTMLElement>(
+      ".curated-playlists-page-layer",
+    );
+    const previousPaddingTop = pageLayer?.style.getPropertyValue("padding-top") ?? "";
+    const previousPaddingTopPriority =
+      pageLayer?.style.getPropertyPriority("padding-top") ?? "";
+
+    pageLayer?.style.setProperty("padding-top", "0px", "important");
 
     function syncHeaderState() {
       window.cancelAnimationFrame(frame);
@@ -43,6 +51,18 @@ export default function CuratedHeaderScrollState() {
       window.removeEventListener("scroll", syncHeaderState);
       headerObserver.disconnect();
       document.body.classList.remove(CURATED_SCROLLED_CLASS);
+
+      if (pageLayer) {
+        if (previousPaddingTop) {
+          pageLayer.style.setProperty(
+            "padding-top",
+            previousPaddingTop,
+            previousPaddingTopPriority,
+          );
+        } else {
+          pageLayer.style.removeProperty("padding-top");
+        }
+      }
     };
   }, []);
 
