2a68b432b96e23a38b097b2f505e4d9abae34697
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-07-13 01:11:47 -0700

    Restore standard curated playlists layout

--- apps/web/app/curated-playlists/layout.tsx
+++ apps/web/app/curated-playlists/layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
-import DiscoverLayout from "../discover/layout";
 import "./curated-playlists.css";
 
 export default function CuratedPlaylistsLayout({ children }: { children: ReactNode }) {
-  return <DiscoverLayout>{children}</DiscoverLayout>;
+  return <>{children}</>;
 }
