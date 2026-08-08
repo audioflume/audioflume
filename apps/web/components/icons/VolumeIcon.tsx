442c5ca8a42947e6e77e2b3dfa5cfc9390d739a3
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-08 09:33:35 -0700

    Thin volume icon strokes further

--- apps/web/components/icons/VolumeIcon.tsx
+++ apps/web/components/icons/VolumeIcon.tsx
@@ -21,7 +21,7 @@ export default function VolumeIcon({
       <path
         d="M2.75 8H7.5L13.25 3.5V20.5L7.5 16H2.75V8Z"
         stroke="currentColor"
-        strokeWidth="1.4"
+        strokeWidth="1.1"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -31,13 +31,13 @@ export default function VolumeIcon({
           <path
             d="M16.25 8L22 16"
             stroke="currentColor"
-            strokeWidth="1.4"
+            strokeWidth="1.1"
             strokeLinecap="round"
           />
           <path
             d="M22 8L16.25 16"
             stroke="currentColor"
-            strokeWidth="1.4"
+            strokeWidth="1.1"
             strokeLinecap="round"
           />
         </>
@@ -46,13 +46,13 @@ export default function VolumeIcon({
           <path
             d="M16 7.5C18.5 10 18.5 14 16 16.5"
             stroke="currentColor"
-            strokeWidth="1.4"
+            strokeWidth="1.1"
             strokeLinecap="round"
           />
           <path
             d="M18.75 4.75C22.75 8.75 22.75 15.25 18.75 19.25"
             stroke="currentColor"
-            strokeWidth="1.4"
+            strokeWidth="1.1"
             strokeLinecap="round"
           />
         </>
