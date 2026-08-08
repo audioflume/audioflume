b6e71fe57930555412f7e64b0ba84cdd825a4f1f
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-08 02:28:06 -0700

    Restore curated Jump Back In section

--- apps/web/app/curated-playlists/layout.tsx
+++ apps/web/app/curated-playlists/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import CuratedFeaturedCarouselControls from "./CuratedFeaturedCarouselControls";
+import CuratedJumpBackIn from "./CuratedJumpBackIn";
 import CuratedPlaylistsLoadingStyles from "./CuratedPlaylistsLoadingStyles";
 import "./curated-playlists.css";
 
@@ -26,6 +27,7 @@ export default function CuratedPlaylistsLayout({
       <CuratedPlaylistsLoadingStyles />
       <style>{CURATED_DETAIL_STYLE}</style>
       <CuratedFeaturedCarouselControls />
+      <CuratedJumpBackIn />
       {children}
     </>
   );
