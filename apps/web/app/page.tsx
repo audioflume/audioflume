3ec4e97832f628273e591a4386fbfcbd59f8c751
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-08 17:33:42 -0700

    Decouple homepage from Discover template

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,5 @@
 import HomePageContent from "./HomePageContent";
-import DiscoverLayout from "./discover/layout";
-import DiscoverTemplate from "./discover/template";
 
 export default function Home() {
-  return (
-    <DiscoverLayout>
-      <DiscoverTemplate showHeroLastViewed={false}>
-        <HomePageContent />
-      </DiscoverTemplate>
-    </DiscoverLayout>
-  );
+  return <HomePageContent />;
 }
