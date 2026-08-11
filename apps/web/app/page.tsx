5c0905226f592e63478726ae8446cd56ce89167d
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-11 03:14:55 -0700

    Remove homepage video playback control

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,5 @@
 import HomePageContent from "./HomePageContent";
-import HomeVideoPlaybackControl from "./HomeVideoPlaybackControl";
 
 export default function Home() {
-  return (
-    <>
-      <HomePageContent />
-      <HomeVideoPlaybackControl />
-    </>
-  );
+  return <HomePageContent />;
 }
