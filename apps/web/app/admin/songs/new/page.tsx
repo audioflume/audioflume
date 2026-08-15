7ef128439849c7a09aff5f61acc302ebdf094b3c
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-15 04:48:03 -0700

    Apply song upload presentation refinements

--- apps/web/app/admin/songs/new/page.tsx
+++ apps/web/app/admin/songs/new/page.tsx
@@ -2,11 +2,13 @@ import AdminSongForm from '@/components/admin/AdminSongForm'
 import AdminSongAiGeneratedFieldInjector from '@/components/admin/AdminSongAiGeneratedFieldInjector'
 import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
 import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
+import AdminSongUploadPresentationInjector from '@/components/admin/AdminSongUploadPresentationInjector'
 
 export default function NewSongPage() {
   return (
     <>
       <AdminSongForm mode="create" />
+      <AdminSongUploadPresentationInjector />
       <AdminSongAiGeneratedFieldInjector />
       <AdminSongRegionFieldInjector />
       <AdminSongFormEditPointsLinkInjector />
