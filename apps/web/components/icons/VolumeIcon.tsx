7a75915bcb9d9917fff286d381719b2bc7ff8474
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-15 18:35:15 -0700

    Restore player volume icon weight

--- apps/web/components/icons/VolumeIcon.tsx
+++ apps/web/components/icons/VolumeIcon.tsx
@@ -10,7 +10,7 @@ export default function VolumeIcon({
   muted = false,
 }: VolumeIconProps) {
   const strokeColor = "var(--filmwave-player-action-icon-color, currentColor)";
-  const strokeWidth = 1.1;
+  const strokeWidth = 1.5;
   const strokeStyle = { strokeWidth };
 
   return (
