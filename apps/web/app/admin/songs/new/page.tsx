42df2fad37c31dc1cd9917da1dc8acac5f6d330d
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-17 03:06:14 -0700

    Clear cover preview for new song uploads

--- apps/web/app/admin/songs/new/page.tsx
+++ apps/web/app/admin/songs/new/page.tsx
@@ -3,12 +3,14 @@ import AdminSongAiGeneratedFieldInjector from '@/components/admin/AdminSongAiGen
 import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
 import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
 import AdminSongUploadPresentationInjector from '@/components/admin/AdminSongUploadPresentationInjector'
+import AdminSongUploadResetSync from '@/components/admin/AdminSongUploadResetSync'
 
 export default function NewSongPage() {
   return (
     <>
       <AdminSongForm mode="create" />
       <AdminSongUploadPresentationInjector />
+      <AdminSongUploadResetSync />
       <AdminSongAiGeneratedFieldInjector />
       <AdminSongRegionFieldInjector />
       <AdminSongFormEditPointsLinkInjector />
