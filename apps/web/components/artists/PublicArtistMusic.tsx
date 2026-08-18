e79da5f2ac8cd65f84a97c09d9ef9a23684aa352
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-18 15:16:41 -0700

    Align artist song list styling

--- apps/web/components/artists/PublicArtistMusic.tsx
+++ apps/web/components/artists/PublicArtistMusic.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { MusicListShell } from "@filmwave/shared";
 import { useEffect } from "react";
 
+import "@/app/music/music-library-redesign.css";
 import SongCard from "@/components/SongCard";
 import { usePlayer } from "@/context/PlayerContext";
 import type { Song } from "@/lib/types";
@@ -14,10 +16,12 @@ export default function PublicArtistMusic({ songs }: { songs: Song[] }) {
   }, [setQueue, songs]);
 
   return (
-    <div>
-      {songs.map((song) => (
-        <SongCard key={song.id} song={song} />
-      ))}
+    <div style={{ margin: "0 var(--filmwave-page-gutter)" }}>
+      <MusicListShell title={null}>
+        {songs.map((song) => (
+          <SongCard key={song.id} song={song} />
+        ))}
+      </MusicListShell>
     </div>
   );
 }
