43e4efc91439d1fd7dd182c4a854ca2c1efbfae8
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-19 14:44:35 -0700

    Remove legacy edit upload adapter

--- apps/web/app/admin/songs/[id]/edit/page.tsx
+++ apps/web/app/admin/songs/[id]/edit/page.tsx
@@ -1,9 +1,7 @@
 import AdminSongAiGeneratedFieldInjector from '@/components/admin/AdminSongAiGeneratedFieldInjector'
-import AdminSongEditUploadStateAdapter from '@/components/admin/AdminSongEditUploadStateAdapter'
 import AdminSongForm from '@/components/admin/AdminSongForm'
 import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
 import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
-import AdminSongUploadPresentationInjector from '@/components/admin/AdminSongUploadPresentationInjector'
 
 export default async function EditSongPage({
   params,
@@ -15,8 +13,6 @@ export default async function EditSongPage({
   return (
     <>
       <AdminSongForm mode="edit" songId={id} />
-      <AdminSongEditUploadStateAdapter />
-      <AdminSongUploadPresentationInjector />
       <AdminSongAiGeneratedFieldInjector songId={id} />
       <AdminSongRegionFieldInjector songId={id} />
       <AdminSongFormEditPointsLinkInjector songId={id} />
