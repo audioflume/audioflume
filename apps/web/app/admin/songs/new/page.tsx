a66278520594a87b5ac37c27dc366efd8560ccbe
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-19 14:44:28 -0700

    Remove legacy admin upload injector

--- apps/web/app/admin/songs/new/page.tsx
+++ apps/web/app/admin/songs/new/page.tsx
@@ -2,15 +2,11 @@ import AdminSongForm from '@/components/admin/AdminSongForm'
 import AdminSongAiGeneratedFieldInjector from '@/components/admin/AdminSongAiGeneratedFieldInjector'
 import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
 import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
-import AdminSongUploadPresentationInjector from '@/components/admin/AdminSongUploadPresentationInjector'
-import AdminSongUploadResetSync from '@/components/admin/AdminSongUploadResetSync'
 
 export default function NewSongPage() {
   return (
     <>
       <AdminSongForm mode="create" />
-      <AdminSongUploadPresentationInjector />
-      <AdminSongUploadResetSync />
       <AdminSongAiGeneratedFieldInjector />
       <AdminSongRegionFieldInjector />
       <AdminSongFormEditPointsLinkInjector />
