f90438812f0d175943de5fb19a80f73bab51dfda
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-19 16:09:46 -0700

    Use integrated admin song form

--- apps/web/app/admin/songs/new/page.tsx
+++ apps/web/app/admin/songs/new/page.tsx
@@ -1,15 +1,5 @@
 import AdminSongForm from '@/components/admin/AdminSongForm'
-import AdminSongAiGeneratedFieldInjector from '@/components/admin/AdminSongAiGeneratedFieldInjector'
-import AdminSongFormEditPointsLinkInjector from '@/components/admin/AdminSongFormEditPointsLinkInjector'
-import AdminSongRegionFieldInjector from '@/components/admin/AdminSongRegionFieldInjector'
 
 export default function NewSongPage() {
-  return (
-    <>
-      <AdminSongForm mode="create" />
-      <AdminSongAiGeneratedFieldInjector />
-      <AdminSongRegionFieldInjector />
-      <AdminSongFormEditPointsLinkInjector />
-    </>
-  )
+  return <AdminSongForm mode="create" />
 }
