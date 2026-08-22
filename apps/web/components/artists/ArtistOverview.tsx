74bccf7184862460287a71e66595613e441cf942
filmwave-webmaster <filmwave.webmaster@gmail.com>
2026-08-21 18:35:15 -0700

    Match overview notification view button

--- apps/web/components/artists/ArtistOverview.tsx
+++ apps/web/components/artists/ArtistOverview.tsx
@@ -870,14 +870,14 @@ export default function ArtistOverview({
                     <button
                       type="button"
                       onClick={() => void handleNotificationView(notification)}
-                      className="filmwave-backend-button filmwave-backend-button-compact"
+                      className="filmwave-backend-button filmwave-backend-button-secondary filmwave-backend-button-compact"
                     >
                       View
                     </button>
                   ) : null}
                 </div>
               </div>
-            ))
+            ))}
           )}
         </section>
       </div>
