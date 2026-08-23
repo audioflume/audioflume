4fada68414f160192bdad003ae702308f1cb297b
audioflume <filmwave.webmaster@gmail.com>
2026-08-23 15:27:38 -0700

    Add backend-style account sidebar

---
+++ apps/web/components/account/AccountSidebar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  BackendSidebarGroup,
+  BackendSidebarHeading,
+  BackendSidebarNavItem,
+  BackendSidebarScrollArea,
+  BackendSidebarShell,
+} from "@/components/backend/BackendSidebar";
+import { usePlayer } from "@/context/PlayerContext";
+import { navItems } from "./accountData";
+
+export default function AccountSidebar() {
+  const pathname = usePathname();
+  const { currentSong } = usePlayer();
+
+  return (
+    <BackendSidebarShell bottom={currentSong ? "64px" : "0px"}>
+      <BackendSidebarScrollArea>
+        <div className="border-b border-[var(--border)] pb-8">
+          <BackendSidebarHeading>Account</BackendSidebarHeading>
+          <div className="flex flex-col gap-px">
+            {navItems.map((item) => (
+              <BackendSidebarNavItem
+                key={item.href}
+                href={item.href}
+                active={pathname === item.href}
+              >
+                {item.label}
+              </BackendSidebarNavItem>
+            ))}
+          </div>
+        </div>
+
+        <BackendSidebarGroup title="Audioflume" className="mt-8">
+          <BackendSidebarNavItem href="/music">Back to Music</BackendSidebarNavItem>
+        </BackendSidebarGroup>
+      </BackendSidebarScrollArea>
+    </BackendSidebarShell>
+  );
+}
