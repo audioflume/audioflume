2a7a04b526e4f2cad81ba8bca599760495f4f8da
audioflume <filmwave.webmaster@gmail.com>
2026-08-29 23:07:12 -0700

    Restore pricing hero flash speed

--- apps/web/app/pricing/PricingHeroImageFlash.tsx
+++ apps/web/app/pricing/PricingHeroImageFlash.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useEffect, useMemo, useState } from "react";
 
-const FLASH_INTERVAL_MS = 100;
+const FLASH_INTERVAL_MS = 140;
 
 type PricingHeroImageFlashProps = {
   images: string[];
